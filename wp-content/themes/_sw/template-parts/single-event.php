ea5d43f2cae00680cd39008ace8c831e32ddb252
Blake Boucher <blakeboucher@gmail.com>
2018-09-06 22:50:01 -0500

    event

--- wp-content/themes/_sw/template-parts/single-event.php
+++ wp-content/themes/_sw/template-parts/single-event.php
@@ -1,6 +1,51 @@
 <article id="post-<?php the_ID(); ?>" <?php post_class('event-single'); ?>>
   <?php
-  get_template_part('template-parts/banner');
-  echo do_shortcode('[text]<h3 class="event-start-time">Start: ' . get_post_meta(get_the_ID(), '_event-start-date', true) . ' @ ' . get_post_meta(get_the_ID(), '_event-start-time', true) . '</h3><h3 class="event-end-time">End: ' . get_post_meta(get_the_ID(), '_event-end-date', true) . ' @ ' . get_post_meta(get_the_ID(), '_event-end-time', true) . '</h3>' . get_the_content() . '[/text]'); ?>
-  <?= do_shortcode('[google_map location="' . get_post_meta(get_the_ID(), '_event-location', true) . '"]'); ?>
+  get_template_part('template-parts/banner'); ?>
+  <section>
+    <div class="container row">
+      <div class="col-lg-8">
+        <div class="text">
+          <?php
+          the_content(); ?>
+          <hr />
+          <?= do_shortcode('[share_buttons buttons="facebook, twitter"]'); ?>
+        </div>
+      </div>
+      <div class="col-lg-4">
+        <div class="info">
+          <h3>Event Details</h3>
+          <p>
+            <b>Date &amp; Time</b>
+            <br />
+            <?php
+            $date = get_post_meta(get_the_ID(), '_event-start-date', true);
+            echo date('l, F jS', strtotime($date)); ?>
+            <br />
+            <?= get_post_meta(get_the_ID(), '_event-start-time', true); ?>
+          </p>
+          <p>
+            <b>Location</b>
+            <?= get_post_meta(get_the_ID(), '_event-location', true); ?>
+          </p>
+          <a href="#" class="btn">Get Tickets</a>
+          <div class="google-map">
+            <div class="map-canvas"></div>
+            <div class="location">
+
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+  <section class="orange">
+    <div class="container row">
+      <div class="col-md-6">
+
+      </div>
+      <div class="col-md-6">
+
+      </div>
+    </div>
+  </section>
 </article>
