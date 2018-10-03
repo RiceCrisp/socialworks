e72afd3c9b41dec184a30eba03bb3303cdb141d8
Blake Boucher <blakeboucher@gmail.com>
2018-10-02 23:23:30 -0500

    default page

--- wp-content/themes/_sw/template-parts/page.php
+++ wp-content/themes/_sw/template-parts/page.php
@@ -1,6 +1,6 @@
 <section id="post-<?php the_ID(); ?>" <?php post_class('page page-content'); ?>>
   <div class="container row">
-    <div class="col-xs-12">
+    <div class="col-xl-8 col-lg-10">
       <?php the_content(); ?>
     </div>
   </div>
