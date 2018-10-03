c2caf31b00f46006ab36b14531fc5d7a37ce831b
Blake Boucher <blakeboucher@gmail.com>
2018-10-02 23:25:20 -0500

    default page

--- wp-content/themes/_sw/template-parts/page.php
+++ wp-content/themes/_sw/template-parts/page.php
@@ -1,6 +1,6 @@
 <section id="post-<?php the_ID(); ?>" <?php post_class('page page-content'); ?>>
   <div class="container row">
-    <div class="col-xl-8 col-lg-10">
+    <div class="col-xl-8 col-xl-offset-2 col-lg-10 col-lg-offset-1">
       <?php the_content(); ?>
     </div>
   </div>
