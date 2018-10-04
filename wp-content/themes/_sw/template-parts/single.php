ab75653c75ed23bde3615bba8f272b4e69d84388
Blake Boucher <blakeboucher@gmail.com>
2018-10-03 21:19:39 -0500

    update

--- wp-content/themes/_sw/template-parts/single.php
+++ wp-content/themes/_sw/template-parts/single.php
@@ -1,5 +1,15 @@
-<article id="post-<?php the_ID(); ?>" <?php post_class('post-single'); ?>>
+<article id="post-<?php the_ID(); ?>" <?php post_class('event-single'); ?>>
   <?php
-  get_template_part('template-parts/banner');
-  echo do_shortcode('[text]' . get_the_content() . '[/text]'); ?>
+  get_template_part('template-parts/banner'); ?>
+  <section>
+    <div class="container row">
+      <div class="col-lg-8">
+        <div class="text">
+          <?php the_content(); ?>
+          <hr />
+          <?= do_shortcode('[share_buttons buttons="facebook, twitter"]'); ?>
+        </div>
+      </div>
+    </div>
+  </section>
 </article>
