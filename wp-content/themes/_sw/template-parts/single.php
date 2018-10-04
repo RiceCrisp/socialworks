ce6712f31d0b8479ea33a0f32dd8d876480b902c
Blake Boucher <blakeboucher@gmail.com>
2018-10-03 21:20:50 -0500

    update

--- wp-content/themes/_sw/template-parts/single.php
+++ wp-content/themes/_sw/template-parts/single.php
@@ -3,7 +3,7 @@
   get_template_part('template-parts/banner'); ?>
   <section>
     <div class="container row">
-      <div class="col-lg-8">
+      <div class="col-xl-8 col-xl-offset-2 col-lg-10 col-lg-offset-1">
         <div class="text">
           <?php the_content(); ?>
           <hr />
